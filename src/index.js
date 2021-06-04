69766276022b76b898268ce47dde968248128123
ShyeChern <chern-97@hotmail.com>
2021-06-04 09:27:05 +0800

    test live

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // css assets
-import 'assets/scss/light-bootstrap-dashboard-react.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/animate.min.css';
+import './assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0';
+import './assets/css/demo.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 // Custom Error boundary for whole app and ui
 import { ErrorBoundary } from 'react-error-boundary';
